6fe6950e1c8c6cba5a1f8202be586160141578b8
prashantkusahis <kumarprashantsahis@gmail.com>
2024-02-26 21:04:19 +0530

    new commit

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ const scroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
   smooth: true,
 });
+
+let elemC = document.querySelector("#elemContainer");
+let fixI = document.querySelector("#fixedImage");
+
+elemC.addEventListener("mouseenter", () => {
+  fixI.style.display = "block";
+});
+elemC.addEventListener("mouseleave", () => {
+  fixI.style.display = "none";
+});
