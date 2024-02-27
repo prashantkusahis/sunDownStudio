289dd724f33da8bb18a0675ed327ad5fddff874f
prashantkusahis <kumarprashantsahis@gmail.com>
2024-02-27 19:38:09 +0530

    new update

--- script.js
+++ script.js
@@ -13,39 +13,10 @@ elemC.addEventListener("mouseleave", () => {
   fixI.style.display = "none";
 });
 
-let elem1 = document.querySelector("#elem1");
-let elem2 = document.querySelector("#elem2");
-let elem3 = document.querySelector("#elem3");
-let elem4 = document.querySelector("#elem4");
-let elem5 = document.querySelector("#elem5");
-let elem6 = document.querySelector("#elem6");
-let elem7 = document.querySelector("#elem7");
-
-elem1.addEventListener("mouseenter", () => {
-  let image = elem1.getAttribute("imageUrl");
-  fixI.style.backgroundImage = `url(${image})`;
-});
-elem2.addEventListener("mouseenter", () => {
-  let image = elem2.getAttribute("imageUrl");
-  fixI.style.backgroundImage = `url(${image})`;
-});
-elem3.addEventListener("mouseenter", () => {
-  let image = elem3.getAttribute("imageUrl");
-  fixI.style.backgroundImage = `url(${image})`;
-});
-elem4.addEventListener("mouseenter", () => {
-  let image = elem4.getAttribute("imageUrl");
-  fixI.style.backgroundImage = `url(${image})`;
-});
-elem5.addEventListener("mouseenter", () => {
-  let image = elem5.getAttribute("imageUrl");
-  fixI.style.backgroundImage = `url(${image})`;
-});
-elem6.addEventListener("mouseenter", () => {
-  let image = elem6.getAttribute("imageUrl");
-  fixI.style.backgroundImage = `url(${image})`;
-});
-elem7.addEventListener("mouseenter", () => {
-  let image = elem7.getAttribute("imageUrl");
-  fixI.style.backgroundImage = `url(${image})`;
+let elems = document.querySelectorAll(".elem");
+elems.forEach((e) => {
+  e.addEventListener("mouseenter", () => {
+    let image = e.getAttribute("imageUrl");
+    fixI.style.backgroundImage = `url(${image})`;
+  });
 });
